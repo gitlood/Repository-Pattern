4f51e6a847873d73f8e52131d7be4f732ab33b50
gitlood <unilood@gmail.com>
2022-08-21 08:59:06 +1200

    Complete

--- app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -22,6 +22,7 @@ import com.example.android.devbyteviewer.database.VideosDatabase
 import com.example.android.devbyteviewer.database.asDomainModel
 import com.example.android.devbyteviewer.domain.DevByteVideo
 import com.example.android.devbyteviewer.network.DevByteNetwork
+import com.example.android.devbyteviewer.network.asDatabaseModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -29,12 +30,16 @@ import kotlinx.coroutines.withContext
  * Repository for fetching devbyte videos from the network and storing them on disk
  */
 class VideosRepository(private val database: VideosDatabase) {
+
     val videos: LiveData<List<DevByteVideo>> = Transformations.map(database.videoDao.getVideos()) {
         it.asDomainModel()
     }
+
     suspend fun refreshVideos() {
         withContext(Dispatchers.IO) {
             val playlist = DevByteNetwork.devbytes.getPlaylist()
+            database.videoDao.insertAll(playlist.asDatabaseModel())
         }
     }
+
 }
